6679ff64cf6adad7f9fb3d887e6dd1df715d3775
natthawut-s <gas.natthawut@gmail.com>
2026-01-12 00:58:40 +0700

    create vite frontend project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <button className="bg-neutral-800 hover:bg-blue-400 text-white border border-blue-300 rounded transition-all duration-1000">
+        Hello Click Me!
+      </button>
+    </>
+  )
+}
+
+export default App
