22af4b0e6a4b65859b90a18bc79cc632340334c6
natthawut-s <gas.natthawut@gmail.com>
2026-01-27 22:55:10 +0700

    skill set paragraph: make it center and equals when display lower than md, and justify-evenly when display as md or more than

--- my-cv-project-frontend/src/pages/client/cv-templates/templates/template1/components/SkillSetParagraph.tsx
+++ my-cv-project-frontend/src/pages/client/cv-templates/templates/template1/components/SkillSetParagraph.tsx
@@ -2,16 +2,21 @@ import React, { useMemo } from "react";
 import type { Skills, SkillSet } from "../../../../../../services/model/mockup.model";
 import type { TemplatePageProps } from "../../template.interface";
 
+const SIDE: { LEFT: string; RIGHT: string } = {
+    LEFT: "L",
+    RIGHT: "R",
+}
+
 const SkillSetParagraph: React.FC<TemplatePageProps> = ({ data }) => {
-    const skillSetLeftSide = useMemo(() => data?.skillSet?.filter((item: SkillSet) => item?.paragraphSide == "L").sort((item: SkillSet) => item?.sequence), [data]);
-    const skillSetRightSide = useMemo(() => data?.skillSet?.filter((item: SkillSet) => item?.paragraphSide == "R").sort((item: SkillSet) => item?.sequence), [data]);
+    const skillSetLeftSide = useMemo(() => data?.skillSet?.filter((item: SkillSet) => item?.paragraphSide == SIDE.LEFT).sort((item: SkillSet) => item?.sequence), [data]);
+    const skillSetRightSide = useMemo(() => data?.skillSet?.filter((item: SkillSet) => item?.paragraphSide == SIDE.RIGHT).sort((item: SkillSet) => item?.sequence), [data]);
 
     const renderSkillSet = (skillSet: SkillSet[], side: string) => {
         return skillSet?.map((item: SkillSet, skillSetIndex: number) => (
-            <React.Fragment key={`${side}_skillset_${skillSetIndex}`}>
-                <p>{item?.skillSetName}</p>
+            <div key={`${side}_skillset_${skillSetIndex}`} className="flex flex-col">
+                <span className="text-lg font-bold pb-2 pt-6">{item?.skillSetName}</span>
 
-                <div className="flex flex-col mb-8">
+                <div className="flex flex-col">
                     {item?.skills?.map((skill: Skills, skillIndex: number) => (
                         <div
                             key={`${side}_skill_${skillSetIndex}_${skillIndex}`}
@@ -22,7 +27,7 @@ const SkillSetParagraph: React.FC<TemplatePageProps> = ({ data }) => {
                         </div>
                     ))}
                 </div>
-            </React.Fragment>
+            </div>
         ));
     };
 
@@ -30,18 +35,20 @@ const SkillSetParagraph: React.FC<TemplatePageProps> = ({ data }) => {
     return (
         <>
             <div className="flex flex-col px-10 py-10 min-h-[400px]">
-                <div className="flex flex-col text-2xl font-bold items-center justify-center py-6">
+                <div className="flex flex-col text-2xl font-bold items-center justify-center pt-6">
                     <span>Skills</span>
                     <hr className="w-3/4 sm:w-3/5 md:w-1/2 border-1 border-white mt-4" />
                 </div>
-                <div className="flex flex-col md:flex-row items-start justify-evenly">
-                    <div className="flex flex-col">
-                        {/* Left Side */}
-                        {renderSkillSet(skillSetLeftSide, "L")}
-                    </div>
-                    <div className="flex flex-col">
-                        {/* Right Side */}
-                        {renderSkillSet(skillSetRightSide, "R")}
+                <div className="flex flex-col items-center justify-evenly">
+                    <div className="flex flex-col md:flex-row items-start justify-evenly size-fit md:w-full">
+                        <div className="flex flex-col">
+                            {/* Left Side */}
+                            {renderSkillSet(skillSetLeftSide, SIDE.LEFT)}
+                        </div>
+                        <div className="flex flex-col">
+                            {/* Right Side */}
+                            {renderSkillSet(skillSetRightSide, SIDE.RIGHT)}
+                        </div>
                     </div>
                 </div>
             </div>
