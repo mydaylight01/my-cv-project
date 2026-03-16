825a71d083de3cc1e9298a9c3c8ff84d5be70636
natthawut-s <gas.natthawut@gmail.com>
2026-03-17 02:27:12 +0700

    update download button

--- my-cv-project-frontend/src/service/api/client/healthCheck.ts
+++ my-cv-project-frontend/src/service/api/client/healthCheck.ts
@@ -1,13 +1,14 @@
 import { apiConfigurations } from '../../ApiConfigurations';
 import { HealthCheckApi } from '../../generated-api/apis/HealthCheckApi';
+import type { HealthCheckResponse } from '../../generated-api/models/HealthCheckResponse';
 
 const healthCheckApi = new HealthCheckApi(apiConfigurations);
 
 export const healthCheckService = {
-    async healthCheck(): Promise<void> {
+    async healthCheck(): Promise<HealthCheckResponse> {
         try {
             // TODO: Add api interceptor to handle the request and response
-            await healthCheckApi.healthCheck();
+            return await healthCheckApi.healthCheck();
         } catch (error: unknown) {
             console.error('[error][healthCheck]', error);
             throw error;
